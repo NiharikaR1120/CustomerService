1b141d85c1bea7de6418cd0a622d2d64a5b06e3c
NiharikaR1120 <128529053+NiharikaR1120@users.noreply.github.com>
2023-03-24 21:45:40 -0400

    Inclass coding

---
+++ CustomerApplication/src/main/java/iu/edu/p565/CustomerApplication/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package iu.edu.p565.CustomerApplication.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handleException(IllegalStateException exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleException(MethodArgumentNotValidException exception){
+        String errorMessages = exception.getBindingResult().getFieldErrors().stream().map(error -> error.getDefaultMessage())
+                .collect(java.util.stream.Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(errorMessages);
+    }
+}
